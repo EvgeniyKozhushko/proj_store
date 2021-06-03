02210b45ccecff82b23510ad8596f5a7df09edc1
Evgeniy Kozhushko <d320@mail.ru>
2021-06-03 14:13:20 +0300

    pretty html

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'cities',
 ]
 
@@ -51,7 +52,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'proj.urls'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -119,8 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
